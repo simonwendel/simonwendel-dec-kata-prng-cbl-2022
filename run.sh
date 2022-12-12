fdde93288eff83d7d405c1ddfdcb0192d18da98b
Simon Wendel <mail@simonwendel.se>
2022-12-12 22:03:15 +0100

    chore: add copyright info and file headers

--- run.sh
+++ run.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+
+# Randomize - a middle-square generator in COBOL
+# Copyright (C) 2022  Simon Wendel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 cobc -x -o bin/randomize randomize.cbl prng.cbl prng-seed.cbl
 ./bin/randomize $1
--- tests.sh
+++ tests.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# Randomize - a middle-square generator in COBOL
+# Copyright (C) 2022  Simon Wendel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 cobc -x -o bin/testfx-eq-tests testfx-eq-tests.cbl testfx-eq.cbl
 ./bin/testfx-eq-tests
 
