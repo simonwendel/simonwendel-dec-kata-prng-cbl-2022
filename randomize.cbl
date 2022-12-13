3a37de6aa20d3c7ea015938c794733769c437779
Simon Wendel <mail@simonwendel.se>
2022-12-13 21:53:26 +0100

    fix: increase the number of cycles possible in main prog

--- randomize.cbl
+++ randomize.cbl
@@ -18,12 +18,12 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       77  cycles               PIC 999.
+       77  cycles               PIC 9(9).
        77  prng                 PIC X(4)    VALUE "prng".
        77  seed                 PIC X(9)    VALUE "prng-seed".
        01  prng-state.
-           05 n                 PIC 99      VALUE 6.
-           05 num               PIC 9(6).
+           05 n                 PIC 99      VALUE 3.
+           05 num               PIC 9(3).
 
        PROCEDURE DIVISION.
            ACCEPT cycles FROM COMMAND-LINE.
